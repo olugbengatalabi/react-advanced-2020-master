ccf711b5386f97c9b304fc8b34a1bbea7d53a323
olugbenga talabi <olugbengatalabi@gmail.com>
2022-05-12 18:38:50 +0100

    useeffect basics

--- react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,24 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setvalue] = useState(0);
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `N(${value})`;
+    }
+  }, [value]);
+  // dependency arrat: if left blank, the useeffect only runs on initial render
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setvalue(value + 1)}>clickme</button>
+    </>
+  )
 };
 
 export default UseEffectBasics;
+
+
+// by default, useeffect will run after every render
+// to prevent this add a list of dependencies
