f162ad1bc19ea3348c4e39940323f8f9d6b2ca5b
olugbenga talabi <olugbengatalabi@gmail.com>
2022-05-15 14:35:03 +0100

    controlled inputs

--- react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,53 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    // e is the event object
+    if (firstName && email) {
+      // const person = { firstName: firstName, email: email };
+      // this can be shortened to 
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person]
+      });
+      setFirstName('');
+      setEmail('');
+      console.log(person);
+
+    } else {
+
+    }
+  }
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handlesubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name:</label>
+            <input type="text" name="firstName" id="firstName" value={firstName} onChange={(e)=> setFirstName(e.target.value) }/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" id="email" value={email} onChange={(e)=> setEmail(e.target.value) }/>
+          </div>
+          <button type='submit' className='btn'>Add Person</button>
+        </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return <>
+            <div className="item" key={id}>
+              <h4 >{firstName}</h4>
+              <h4 >{email}</h4>
+            </div>
+          </>
+        })}
+      </article>
+    </>
+  )
 };
 
 export default ControlledInputs;
